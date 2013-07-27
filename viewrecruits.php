2e8678d562bc2805ce3dd68976382426e2de145e
Ryan OSullivan <rosullivan@kobo.com>
2013-07-27 12:59:54 -0400

    view recruits coming along

--- viewrecruits.php
+++ viewrecruits.php
@@ -1,6 +1,6 @@
 <? $pageid='viewrecruits'; include('settings.php'); include('phpheader.php'); include('header.php'); ?>
 
-<h2>View Recruits</h2>
+<h2>View All Recruits</h2>
 
 <table id='viewrecruits'>
 <tr>
@@ -9,19 +9,18 @@
 	<th>Forum Name</th>
 	<th>Status</th>
 	<th>Game</th>
-	<th>Wave</th>
 	<th>Trial Date</th>
 	<th>Trial Days</th>
 	<th>Comments</th>
-	<th>M.Score</th>
 	<th>O.Score</th>
+	<th>M.Score</th>
 </tr>
 <? 
 // Fill table with currently active recruits
 // grab active recruits from DB
 if ($dbR = $db->query("SELECT * FROM recruits")){
 	if ($dbR->num_rows > 0){
-		for($i = 0; $i <= $dbR->num_rows; $i++){
+		for($i = 0; $i < $dbR->num_rows; $i++){
 			$result = $dbR->fetch_assoc();
 			
 			// caclulate number of trial days
@@ -35,17 +34,17 @@ if ($dbR = $db->query("SELECT * FROM recruits")){
 			$trialDate = "{$trialDate->format('Y-m-d')}";
 
 			// display recruit info in a table
-			echo "<tr><td>Vote</td>
+			echo "<tr>
+			<td><img src='img/thumbsup.png'/><img src='img/thumbsdown.png'/></td>
 			<td>{$result['name']}</td>
 			<td>{$result['forumName']}</td>
 			<td>{$result['status']}</td>
 			<td>{$result['gameID']}</td>
-			<td>Wave</td>
 			<td>{$trialDate}</td>
 			<td>{$datediff}</td>
 			<td>Comments</td>
-			<td>M.Score</td>
-			<td>O.Score</td></tr>";	
+			<td>O.Score</td>
+			<td>M.Score</td></tr>";	
 		}
 	}
 }	
